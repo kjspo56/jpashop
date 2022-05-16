e46e3caf5e3fa0f7a307e53bde6ac8fe9acc72f0
kjspo56 <kjspo56@gmail.com>
2022-05-16 22:11:12 +0900

    Entity Class 개발2

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -18,4 +21,8 @@ public abstract class Item {    //구현체를 가지고 할 것이기때문에 
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categoryies = new ArrayList<>();
+
 }
